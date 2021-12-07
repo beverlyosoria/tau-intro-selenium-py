f717f5475f0e6a3a8f3b73b17095bcd179b23eae
Beverly Osoria <beverlyosoria@MacBook-Pro.local>
2021-12-07 10:41:25 -0600

    initial commit

--- tests/test_search.py
+++ tests/test_search.py
@@ -2,21 +2,30 @@
 These tests cover DuckDuckGo searches.
 """
 
-def test_basic_duckduckgo_search():
+from pages.result import DuckDuckGoResultPage
+from pages.search import DuckDuckGoSearchPage
 
-# Given the DuckDuckGo home page is displayed
-# TODO
 
-# When the user searches for "panda"
-# TODO
+def test_basic_duckduckgo_search(browser):
+  search_page = DuckDuckGoSearchPage(browser)
+  result_page = DuckDuckGoResultPage(browser)
+  PHRASE = "panda"
+  
+  # Given the DuckDuckGo home page is displayed
+  search_page.load()
 
-# Then the search result title contains "panda"
-# TODO
-    
-# And the search result query is "panda"
-# TODO
-    
-# And the search result links pertain to "panda"
-# TODO
+  # When the user searches for "panda"
+  search_page.search(PHRASE)
 
-    raise Exception("Incomplete Test")
+  # Then the search result title contains "panda"
+  assert PHRASE in result_page.title()
+  
+  # And the search result query is "panda"
+  assert PHRASE == result_page.search_input_value()
+  
+  # And the search result links pertain to "panda"
+  for title in result_page.result_link_titles():
+    assert PHRASE.lower() in title.lower()
+
+  # TODO: Remove this exception once the test is complete
+  raise Exception("Incomplete Test")
